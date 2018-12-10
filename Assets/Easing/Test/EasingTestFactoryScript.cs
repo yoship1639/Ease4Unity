427386e53f387b82bf8ffb55faa6c397da5fa6b5
Yoshihiro Ito <yoship1639@gmail.com>
2018-12-10 12:11:31 +0900

    created ease base script

---
+++ Assets/Easing/Test/EasingTestFactoryScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TestEaseFactoryScript : MonoBehaviour
+{
+    void Start()
+    {
+        for (var i = 0; i < 32; i++)
+        {
+            var go = Resources.Load<GameObject>("Sphere");
+            Instantiate(go);
+            var s = go.GetComponent<EasingTestMoveScript>();
+            s.From = new Vector3(-16, 0.0f, 16 - i);
+            s.To = new Vector3(16, 0.0f, 16 - i);
+            s.Type = (EaseType)i;
+        }
+    }
+}
---
+++ Assets/Easing/Test/EasingTestMoveScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EasingTestMoveScript : MonoBehaviour
+{
+    public EaseType Type = EaseType.Linear;
+    public Vector3 From;
+    public Vector3 To;
+    public float Interval = 2.0f;
+
+    bool flag;
+    float time;
+    float wait;
+
+    void Update()
+    {
+        if (wait <= 0.0) time += Time.deltaTime;
+        else wait -= Time.deltaTime;
+
+        if (time > Interval)
+        {
+            wait = 1.0f;
+            time = 0.0f;
+            flag = !flag;
+            if (flag) transform.position = From;
+            else transform.position = To;
+        }
+
+        if (wait <= 0.0f)
+        {
+            if (flag) transform.position = Vector3.LerpUnclamped(From, To, (time / Interval).Easing(Type));
+            else transform.position = Vector3.LerpUnclamped(To, From, (time / Interval).Easing(Type));
+        }
+    }
+}
