168f8c07561ca9863e4d3df7eaca9b414d20a98a
Yoshihiro Ito <yoship1639@gmail.com>
2018-12-11 01:41:16 +0900

    add test scene

---
+++ Assets/Ease4Unity/Test/EasingTestRotScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EasingTestRotScript : MonoBehaviour
+{
+    public EaseType Type = EaseType.Linear;
+    public Quaternion From = Quaternion.identity;
+    public Quaternion To = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+    public float Interval = 2.0f;
+
+    bool flag;
+    float time;
+    float wait;
+
+    void Update()
+    {
+        if (wait <= 0.0) time += Time.deltaTime;
+        else wait -= Time.deltaTime;
+
+        if (time > Interval)
+        {
+            wait = 1.0f;
+            time = 0.0f;
+            flag = !flag;
+            if (flag) transform.rotation = From;
+            else transform.rotation = To;
+        }
+
+        if (wait <= 0.0f)
+        {
+            if (flag) transform.rotation = Quaternion.SlerpUnclamped(From, To, (time / Interval).Easing(Type));
+            else transform.rotation = Quaternion.SlerpUnclamped(To, From, (time / Interval).Easing(Type));
+        }
+    }
+}
