d309304cd44ad974ad694d7d713ea7a2314fe429
Yoshihiro Ito <yoship1639@gmail.com>
2018-12-11 10:16:40 +0900

    fix test rot script, add scale script

--- Assets/Ease4Unity/Test/EasingTestRotScript.cs
+++ Assets/Ease4Unity/Test/EasingTestRotScript.cs
@@ -3,8 +3,8 @@
 public class EasingTestRotScript : MonoBehaviour
 {
     public EaseType Type = EaseType.Linear;
-    public Quaternion From = Quaternion.identity;
-    public Quaternion To = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+    public Vector3 From = Vector3.zero;
+    public Vector3 To = new Vector3(0.0f, 90.0f, 0.0f);
     public float Interval = 2.0f;
 
     bool flag;
@@ -21,14 +21,12 @@ public class EasingTestRotScript : MonoBehaviour
             wait = 1.0f;
             time = 0.0f;
             flag = !flag;
-            if (flag) transform.rotation = From;
-            else transform.rotation = To;
+            transform.rotation = Quaternion.Euler(From);
         }
 
         if (wait <= 0.0f)
         {
-            if (flag) transform.rotation = Quaternion.SlerpUnclamped(From, To, (time / Interval).Easing(Type));
-            else transform.rotation = Quaternion.SlerpUnclamped(To, From, (time / Interval).Easing(Type));
+            transform.rotation = Quaternion.SlerpUnclamped(Quaternion.Euler(From), Quaternion.Euler(To), (time / Interval).Easing(Type));
         }
     }
 }
---
+++ Assets/Ease4Unity/Test/EasingTestScaleScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EasingTestScaleScript : MonoBehaviour
+{
+    public EaseType Type = EaseType.Linear;
+    public Vector3 From = Vector3.one;
+    public Vector3 To = Vector3.one * 0.5f;
+    public float Interval = 2.0f;
+
+    bool flag;
+    float time;
+    float wait;
+
+    void Update()
+    {
+        if (wait <= 0.0) time += Time.deltaTime;
+        else wait -= Time.deltaTime;
+
+        if (time > Interval)
+        {
+            wait = 1.0f;
+            time = 0.0f;
+            flag = !flag;
+            if (flag) transform.localScale = From;
+            else transform.localScale = To;
+        }
+
+        if (wait <= 0.0f)
+        {
+            if (flag) transform.localScale = Vector3.LerpUnclamped(From, To, (time / Interval).Easing(Type));
+            else transform.localScale = Vector3.LerpUnclamped(To, From, (time / Interval).Easing(Type));
+        }
+    }
+}
